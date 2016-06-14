bc0a393331b149085a6c53fb78e42a3b0d3a99bc
Joseba S <jjoseba@gmail.com>
2016-06-14 20:13:34 +0200

    Moved the xsl file to its own folder

--- src/settings.py
+++ src/settings.py
@@ -2,4 +2,4 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 TEMPLATES_ROOT = os.path.join(PROJECT_ROOT, 'templates')
-XSL_FILE = os.path.join(PROJECT_ROOT, 'coursetemplate.xsl')
+XSL_FILE = os.path.join(PROJECT_ROOT, 'xsl/openlearn2html.xsl')
